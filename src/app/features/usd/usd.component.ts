a177354fb08056734f66814b2877bb52bc66affd
franbarreneche <franbarreneche@gmail.com>
2023-07-15 20:05:53 -0300

    feat: adds historic prices

--- src/app/features/usd/usd.component.ts
+++ src/app/features/usd/usd.component.ts
@@ -14,7 +14,7 @@ import { UsdStoreService } from './usd-store.service';
     <div class="flex flex-col items-center justify-start w-full h-full">
 
       <ng-container *ngIf="dolars$ | async as dolars">
-        <div class="stats shadow">
+        <div class="stats shadow w-full">
           <div class="stat">
             <div class="stat-title">OFICIAL</div>            
             <div class="stat-value">{{ dolars.oficial.price | currency:'ARS':'symbol-narrow':'1.0-0'}}</div>
@@ -38,39 +38,35 @@ import { UsdStoreService } from './usd-store.service';
         </div>
       </ng-container>
 
-      <ng-container *ngIf="seriesSubject | async as series">
-        <div class="card bg-base-100 shadow-xl grow">        
+      <ng-container *ngIf="series$ | async as series">
+        <div class="card bg-base-100 shadow-xl grow w-full">        
           <app-chart-timeline [series]="series"/>
           <div class="card-body">
-            <h2 class="card-title">Shoes!</h2>   
+            <h2 class="card-title">Precios Último Año</h2>   
             
             <!-- TABLE -->
-            <div class="overflow-x-auto overflow-y-scroll">
+            <div class="overflow-x-auto overflow-y-auto">
               <table class="table">
                 <!-- head -->
                 <thead>
                   <tr>
                     <th>Date</th>
-                    <th>Oficial</th>
-                    <th>Blue</th>
+                    <th *ngFor="let serie of series">{{ serie.name }}</th>
                   </tr>
                 </thead>
                 <tbody>
                   <ng-container *ngIf="series.length !== 0">
                     <tr class="hover" *ngFor="let datapoint of series[0].data; index as i">
                       <th>{{ datapoint[0] | date}}</th>
-                      <th>{{ datapoint[1] | currency:'ARS':'symbol-narrow'}}</th>
-                      <td>{{ series[1].data[i][1] | currency:'ARS':'symbol-narrow'}}</td>
+                      <th *ngFor="let serie of series">
+                        <span *ngIf="serie.data[i]?.length === 2">{{ serie.data[i][1] | currency:'ARS':'symbol-narrow'}}</span>
+                      </th>
                     </tr>
                   </ng-container>
                 </tbody>
               </table>
             </div>
             <!-- END TABLE -->
-
-            <div class="card-actions justify-end">
-              <button class="btn btn-primary" (click)="changeData()">Change Data</button>
-            </div>
           </div>
         </div>           
       </ng-container>        
@@ -80,34 +76,8 @@ import { UsdStoreService } from './usd-store.service';
 })
 export class UsdComponent {
   dolars$ = this.store.getActualUsdPrices();
-  seriesSubject = new BehaviorSubject<any>([]);
+  series$ = this.store.getUsdLastYearPrice();
   
   constructor(private store: UsdStoreService){}
-
-  private getRandomData(): number[][] {
-    let base = +new Date(1988, 9, 3);
-    let oneDay = 24 * 3600 * 1000;
-
-    let data = [[base, Math.random() * 300]];
-
-    for (let i = 1; i < 20000; i++) {
-      let now = new Date((base += oneDay));
-      data.push([+now, Math.round((Math.random() - 0.5) * 20 + data[i - 1][1])]);
-    }
-
-    return data;
-  }
-
-  changeData() {
-    const cantSeries = 2;
-    const series = [];
-    for (let index = 0; index < cantSeries; index++) {
-      series.push({
-        name: `Series ${index}`,
-        data: this.getRandomData()
-      });
-    }
-    this.seriesSubject.next(series);
-  }
 
 }

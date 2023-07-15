6c7f2f1ba2454dc6ce66ec64f15e56ed2a305b1d
franbarreneche <franbarreneche@gmail.com>
2023-07-15 17:05:03 -0300

    fix: router links on nav bar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,20 +1,20 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { RouterOutlet } from '@angular/router';
+import { RouterModule, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, RouterModule],
   template: `  
     <div class="flex flex-col h-screen" data-theme="dark">
       <div class="navbar bg-neutral text-neutral-content">
         <a class="btn btn-ghost normal-case text-xl">Swissknife Argy</a>
       </div>
       <ul class="menu menu-vertical lg:menu-horizontal bg-base-200 rounded-box">
-        <li><a>Item 1</a></li>
-        <li><a>Item 2</a></li>
-        <li><a>Item 3</a></li>
+        <li><a routerLink="/usd">USD</a></li>
+        <li><a routerLink="/inflation">Inflation</a></li>
+        <li><a routerLink="/interest-rates">Interest Rates</a></li>
       </ul>
       <div class="grow">
         <router-outlet></router-outlet>
